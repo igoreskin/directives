2da72dbddc8c5bdc4616350d95263cd839e74d45
Igor Eskin <eskinhd@gmail.com>
2019-06-09 17:35:49 -0400

    Structural and attribute directives in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'directives';
+
+  numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
 }
